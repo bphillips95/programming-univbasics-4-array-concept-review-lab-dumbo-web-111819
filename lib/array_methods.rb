018d0326a6080db3e9ce2e47b7fc1003a4f7756a
Baruch Phillips <baruchphillips@gmail.com>
2019-10-30 20:40:12 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
  counter = 0 
   while array[counter] do
-    array.include?([value_to_find])
+   puts array.include?([value_to_find])
     counter +=1
   end
 end
