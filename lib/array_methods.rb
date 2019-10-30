dc10f0f3983a4792b4ffc62d8f01decf70031539
Baruch Phillips <baruchphillips@gmail.com>
2019-10-30 21:06:58 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,10 @@
 def find_element_index(array, value_to_find)
  counter = 0 
   while counter < array.length do
-    array.index[]
+    array.index(value_to_find)
     counter +=1
   end
-  return array[value_to_find]
+  
 end
 
 def find_max_value(array)
